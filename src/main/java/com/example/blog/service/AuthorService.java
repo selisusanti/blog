4463bf35877bd8836c3147601210df169bb46372
selisusanti <seli.susanti@indosystem.com>
2020-04-08 15:10:48 +0700

    hilangin yang tidak perlu

--- src/main/java/com/example/blog/service/AuthorService.java
+++ src/main/java/com/example/blog/service/AuthorService.java
@@ -12,51 +12,11 @@ import org.springframework.data.domain.Pageable;
 public interface AuthorService{
 
     ResponseAuthorDTO save(Author request);
-    // Page<ResponseCommentDTO> findAllByBlogId(Pageable pageable, Long id);
     Page<ResponseAuthorDTO> findAll(Pageable pageable);
     Page<ResponseAuthorDTO> findByName(Pageable pageable, String param);
     ResponseAuthorDTO findById(Long id);
     ResponseAuthorDTO deleteById(DeleteDTO request);
     ResponseAuthorDTO update(Long id, AuthorDTO request);
     ResponseAuthorDTO updatePassword(Long id, AuthorPasswordDTO request);
-    // void deleteAllByPostId (Long id);
-
-    // private final AuthorRepository authorRepository;
-
-    // @Autowired
-    // public AuthorService(AuthorRepository authorRepository){
-    //     this.authorRepository = authorRepository;
-    // }
-
-    // private ResponseAuthorDTO fromEntity(Author author) {
-    //     ResponseAuthorDTO response = new ResponseAuthorDTO();
-    //     BeanUtils.copyProperties(author, response);
-    //     return response;
-    // }
-
-    // public Page<ResponseAuthorDTO> findAll(Pageable pageable){
-    //     try {
-    //         return authorRepository.findAll(pageable).map(this::fromEntity);
-    //     } catch (Exception e) {
-    //         log.error(e.getMessage(), e);
-    //         throw e;
-    //     }
-    // }
- 
-    // public List<Author> findByName(String username){
-    //     return authorRepository.findByUsername(username);
-    // }
-
-    // public Optional<Author> findById(Long id){
-    //     return authorRepository.findById(id);
-    // }
-
-    // public Author save(Author author){
-    //     return authorRepository.save(author);
-    // }
-
-    // public void deleteById(Long id) {
-    //     authorRepository.deleteById(id);
-    // }
-
+    
 }
